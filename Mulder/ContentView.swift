5f61e230c6e6e00df333cb44bbf0856a99be012a
Erik Pragt <erik.pragt@gmail.com>
2019-12-08 00:43:01 +1100

    Initial Commit

---
+++ Mulder/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Mulder
+//
+//  Created by Erik Pragt on 8/12/19.
+//  Copyright © 2019 Erik Pragt. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
