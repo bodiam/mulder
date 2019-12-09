0ad6b40ce56422e93efc1d23df1914b9197c427c
Erik Pragt <erik.pragt@gmail.com>
2019-12-10 10:06:09 +1100

    Added support for 3 random images

--- Mulder/ContentView.swift
+++ Mulder/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {        
-        Image("background")
+        Image("background" + String(Int.random(in: 0 ..< 3)))
     }
 }
 
